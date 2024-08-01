23b3033a9e8adb204d9d96445efac0e2479a871b
nsidir <nicksidiropoulos56@gmail.com>
2024-08-01 22:05:43 +0300

    Fixed stuff for pages deployment

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-emcc main.cpp Graph.cpp -o graphs.html -s USE_SDL=2 -s WASM=1 --preload-file Fonts -s USE_SDL_TTF=2 -s USE_SDL_GFX=2 -O3 -flto -s ASYNCIFY
+emcc main.cpp Graph.cpp -o index.html -s USE_SDL=2 -s WASM=1 --preload-file Fonts -s USE_SDL_TTF=2 -s USE_SDL_GFX=2 -O3 -flto -s ASYNCIFY
